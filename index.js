ff809a649c7cb275979ec22f064dad5d28c443dd
Coby Walsh <cobywalsh7@yahoo.com>
2024-02-06 19:23:00 -0700

    created the head of the snake

--- index.js
+++ index.js
@@ -3,15 +3,29 @@ const ctx = canvas.getContext('2d');
 
 let speed = 7;
 
+let tileCount = 20;
+let tileSize = canvas.width / tileCount - 2;
+let headX = 10;
+let headY = 10;
+
+let xVelocity = 0;
+let yVelocity = 0;
+
 // Game Loop
 function drawGame() {
 clearScreen();
+drawSnake();
 setTimeout(drawGame, 1000/ speed);
 }
 
 function clearScreen() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0,0,canvas.clientWidth,canvas.height);
+}
+
+function drawSnake() {
+    ctx.fillStyle = 'orange'
+    ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
 }
 
 drawGame();
