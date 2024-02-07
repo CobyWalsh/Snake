7e710762b2c93beae991d8d80e290de76d7c6cb5
Coby Walsh <cobywalsh7@yahoo.com>
2024-02-06 19:45:05 -0700

    created an apple

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ let tileSize = canvas.width / tileCount - 2;
 let headX = 10;
 let headY = 10;
 
+let appleX = 5;
+let appleY = 5;
+
 let xVelocity = 0;
 let yVelocity = 0;
 
@@ -15,6 +18,7 @@ let yVelocity = 0;
 function drawGame() {
 clearScreen();
 changeSnakePosition();
+drawApple();
 drawSnake();
 setTimeout(drawGame, 1000/ speed);
 }
@@ -32,6 +36,11 @@ function drawSnake() {
 function changeSnakePosition() {
     headX = headX + xVelocity;
     headY = headY + yVelocity;
+}
+
+function drawApple() {
+    ctx.fillStyle = "red";
+    ctx.fillRect( appleX, appleY, tileSize, tileSize);
 }
 
 document.body.addEventListener('keydown', keyDown);
